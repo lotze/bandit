b0cc728b91e671c8b5ea04d551b3844d0e205cff
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-14 12:23:49 -0800

    adding summarize metrics, correcting rank order for significance analysis

--- R/significance_analysis.R
+++ R/significance_analysis.R
@@ -1,9 +1,12 @@
+# performs overall proportion test; if it is significant at alpha=0.05, performs pairwise tests to see where there are significant differences between proportions
+# n should not be very different between the different groups under test; this is intended for simple analysis of split tests with multiple outcomes
+# result rows are in the same order as the input x and n
 significance_analysis <-
 function(x, n) {
 	num_groups = length(x)
 	
 	p = x/n
-	order = rev(order(p))
+	my_rank = rank(-p)
 
 	pt = prop.test(x=x, n=n)
 	
@@ -15,8 +18,8 @@ function(x, n) {
 	if (pt$p.value < 0.05) {
 		is_best = 1
 		for (cur_rank in (1:(num_groups-1))) {
-			cur_index = which(order==cur_rank)
-			comparison_index = which(order==cur_rank+1)
+			cur_index = which(my_rank==cur_rank)
+			comparison_index = which(my_rank==cur_rank+1)
 			# compare to the next lower proportion
 			pt = prop.test(x=x[c(cur_index, comparison_index)], n=n[c(cur_index, comparison_index)], conf.level = (1 - 0.05))
 			significance[cur_index] = pt$p.value
@@ -29,5 +32,5 @@ function(x, n) {
 		}
 	}
 	
-	return(data.frame(successes=x, totals=n, estimated_proportion=p, lower=lower, upper=upper, significance=significance, order=order, best=best))
+	return(data.frame(successes=x, totals=n, estimated_proportion=p, lower=lower, upper=upper, significance=significance, rank=my_rank, best=best))
 }
---
+++ R/summarize_metrics.R
@@ -0,0 +1,19 @@
+summarize_metrics <- 
+function(metric_list, data_is_binary) {
+	medians = sapply(metric_list, median)
+	means = sapply(metric_list, mean)
+	num_alternatives = length(metric_list)
+	lowers = rep(NA, num_alternatives)
+	uppers = rep(NA, num_alternatives)
+	if (data_is_binary) {
+		cis = sapply(metric_list, function(d) {prop.test(sum(d),length(d))$conf.int})
+		lowers = cis[1,]
+		uppers = cis[2,]
+	} else {
+		cis = sapply(metric_list, function(d) {wilcox.test(d, conf.int=TRUE)$conf.int})
+		lowers = cis[1,]
+		uppers = cis[2,]
+	}
+	
+	return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers))
+}
