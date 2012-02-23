5e388f9c53098e0f6e582fbe52a29cbae7625ec4
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-22 17:55:47 -0800

    no medians for binary data

--- R/summarize_metrics.R
+++ R/summarize_metrics.R
@@ -1,19 +1,19 @@
 summarize_metrics <- 
 function(metric_list, data_is_binary=TRUE) {
-	medians = sapply(metric_list, median)
 	means = sapply(metric_list, mean)
 	num_alternatives = length(metric_list)
 	lowers = rep(NA, num_alternatives)
 	uppers = rep(NA, num_alternatives)
-	if (data_is_binary) {
-		cis = sapply(metric_list, function(d) {prop.test(sum(d),length(d))$conf.int})
-		lowers = cis[1,]
-		uppers = cis[2,]
-	} else {
+	if (!data_is_binary) {
+		medians = sapply(metric_list, median)
 		cis = sapply(metric_list, function(x) {as.vector(quantile(x,c(0.05,0.95)))})
 		lowers = cis[1,]
 		uppers = cis[2,]
+		return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers))
+	} else {
+		cis = sapply(metric_list, function(d) {prop.test(sum(d),length(d))$conf.int})
+		lowers = cis[1,]
+		uppers = cis[2,]
+		return(data.frame(mean=means,lower=lowers,upper=uppers))
 	}
-	
-	return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers))
 }
