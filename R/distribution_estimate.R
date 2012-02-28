1376f194d7915aa28a4b434395688a00bd28ded5
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-28 11:36:26 -0800

    making distribution_estimate consistent between binary and non-binary

--- R/distribution_estimate.R
+++ R/distribution_estimate.R
@@ -4,7 +4,7 @@ function(metric_list, data_is_binary=TRUE) {
 	if (data_is_binary) {
 		sums = sapply(metric_list, function(v) {sum(c(v,0))})
 		totals = sapply(metric_list, function(v) {if (length(v)  > 0) {length(v)} else {0}})
-		retval = sapply(1:length(metric_list), function(i) {pbeta(p_to_examine,sums[i]+1,totals[i]-sums[i]+1)})
+		retval = sapply(1:length(metric_list), function(i) {qbeta(p_to_examine,sums[i]+1,totals[i]-sums[i]+1)})
 	} else {
 		retval = sapply(metric_list, function(x) {as.vector(quantile(x,p_to_examine))})
 	}
