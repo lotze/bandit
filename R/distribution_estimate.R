d1312cd6c9d3fbe03c473899ba28e606b4119752
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-27 16:38:46 -0800

    adding function for distribution estimation

---
+++ R/distribution_estimate.R
@@ -0,0 +1,12 @@
+distribution_estimate <- 
+function(metric_list, data_is_binary=TRUE) {
+	p_to_examine = seq(0,1,by=0.01)
+	if (data_is_binary) {
+		sums = sapply(metric_list, function(v) {sum(c(v,0))})
+		totals = sapply(metric_list, function(v) {if (length(v)  > 0) {length(v)} else {0}})
+		retval = sapply(1:length(metric_list), function(i) {pbeta(p_to_examine,sums[i]+1,totals[i]-sums[i]+1)})
+	} else {
+		retval = sapply(metric_list, function(x) {as.vector(quantile(x,p_to_examine))})
+	}
+	return(retval)
+}
