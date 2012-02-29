47e6de2acd146149f90a12ccf116dd16a67674ed
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-29 13:22:18 -0800

    use type-2 quantiles

--- R/distribution_estimate.R
+++ R/distribution_estimate.R
@@ -6,7 +6,7 @@ function(v, data_is_binary=TRUE, num_quantiles=101) {
 		totals = if (length(v)  > 0) {length(v)} else {0}
 		quantiles = qbeta(p_to_examine,sums+1,totals-sums+1)
 	} else {
-		quantiles = as.vector(quantile(v,p_to_examine))
+		quantiles = as.vector(quantile(v,p_to_examine,type=2))
 	}
 	x = rep(quantiles,each=2)
 	mids = (quantiles[2:length(quantiles)] + quantiles[1:(length(quantiles)-1)])/2.0
