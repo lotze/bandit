6ca6e393b0dc0d5be8bec95a9e5d7a6def7ad3d5
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-02-29 20:15:55 -0800

    adding extra (some redundant) information to summarize_metrics

--- R/summarize_metrics.R
+++ R/summarize_metrics.R
@@ -4,16 +4,18 @@ function(metric_list, data_is_binary=TRUE) {
 	num_alternatives = length(metric_list)
 	lowers = rep(NA, num_alternatives)
 	uppers = rep(NA, num_alternatives)
+	totals = sapply(metric_list, sum)
+	num_obs = sapply(metric_list, length)
 	if (!data_is_binary) {
 		medians = sapply(metric_list, median)
 		cis = sapply(metric_list, function(x) {as.vector(quantile(x,c(0.05,0.95)))})
 		lowers = cis[1,]
 		uppers = cis[2,]
-		return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers))
+		return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers, num_obs=num_obs, total=totals))
 	} else {
 		cis = sapply(metric_list, function(d) {prop.test(sum(d),length(d))$conf.int})
 		lowers = cis[1,]
 		uppers = cis[2,]
-		return(data.frame(mean=means,lower=lowers,upper=uppers))
+		return(data.frame(mean=means,lower=lowers,upper=uppers, num_obs=num_obs, total=totals))
 	}
 }
