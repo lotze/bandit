00cc8499aaf660590607b4f264455fc4ef59880b
Thomas Lotze <thomas.lotze@thomaslotze.com>
2012-04-02 10:11:06 -0700

    updated to 0.1.7: nonbinary distribution and confidence intervals are on the mean now, not individual values

--- R/distribution_estimate.R
+++ R/distribution_estimate.R
@@ -1,12 +1,14 @@
 distribution_estimate <- 
 function(v, data_is_binary=TRUE, num_quantiles=101) {
+	require(boot)
 	p_to_examine = seq(0,1,length.out=num_quantiles)
 	if (data_is_binary) {
 		sums = sum(c(v,0))
 		totals = if (length(v)  > 0) {length(v)} else {0}
 		quantiles = qbeta(p_to_examine,sums+1,totals-sums+1)
 	} else {
-		quantiles = as.vector(quantile(v,p_to_examine,type=2))
+		bootstrapped_means = boot(v, function(y,i) {mean(y[i])}, 1000)
+		quantiles = as.vector(quantile(bootstrapped_means$t,p_to_examine,type=2))
 	}
 	x = rep(quantiles,each=2)
 	mids = (quantiles[2:length(quantiles)] + quantiles[1:(length(quantiles)-1)])/2.0
--- R/summarize_metrics.R
+++ R/summarize_metrics.R
@@ -1,5 +1,6 @@
 summarize_metrics <- 
 function(metric_list, data_is_binary=TRUE) {
+	require(boot)
 	means = sapply(metric_list, mean)
 	num_alternatives = length(metric_list)
 	lowers = rep(NA, num_alternatives)
@@ -8,7 +9,10 @@ function(metric_list, data_is_binary=TRUE) {
 	num_obs = sapply(metric_list, length)
 	if (!data_is_binary) {
 		medians = sapply(metric_list, median)
-		cis = sapply(metric_list, function(x) {as.vector(quantile(x,c(0.05,0.95)))})
+		mean_replicates = lapply(metric_list, function(x) {
+			boot(x, function(y,i) {mean(y[i])}, 1000)
+		})
+		cis = sapply(mean_replicates, function(x) {boot.ci(x, conf=0.95, type="perc")$percent[4:5]})
 		lowers = cis[1,]
 		uppers = cis[2,]
 		return(data.frame(mean=means,median=medians,lower=lowers,upper=uppers, num_obs=num_obs, total=totals))
